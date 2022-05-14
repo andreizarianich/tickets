a31192b2e61fbe275a2c558bfd15d971fdf5dff0
andreizarianich <104719743+andreizarianich@users.noreply.github.com>
2022-05-14 17:16:00 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,31 +1,7 @@
-#include <iostream>
-#include <fstream>
-#include "halls.hpp"
-
-void PrintMenu()
-{
-    std::cout<< "--------- Welcome ---------\n1: Buy a ticket\n2: Reserve a ticket\n3: Check for free seats";
-    std::cout<< "\n4: Cancel reservation\n5: See list of reservations\n6: See list of sold tickets\n7: Add a new event";
-    std::cout<< "\n0: Exit\n---------------------------\n";
-}
-
-char CheckInput()
-{
-    char user_input;
-    std::cout<< "Please choose an option: \n";
-    std::cin.get(user_input);
-    return user_input;
-}
+#include "tickets.hpp"
 
 int main()
 {
-    
-    Halls halls;
-
-    do
-    {
-        PrintMenu();
-    } while (CheckInput()!='0');
-
+    Tickets::Start();
     return 0;
 }
